a933cd4051a6259e1327824b50f0d439d81a1e09
M Prithvi <madivadapr@gmail.com>
2024-08-30 18:51:24 +0530

    added results page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import HeroSection from "./components/HeroSection";
+import ResultPage from "./components/ResultPage";
 import Stats from "./components/Stats";
 import IntroMission from "./components/IntroMission";
 import DeepFakeCases from "./components/DeepFakeCases";
@@ -10,18 +12,31 @@ import Footer from "./components/Footer";
 import "./App.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 
-function App() {
+function HomePage() {
   return (
-    <div className="App">
-      <Header />
+    <>
       <HeroSection />
       <Stats />
       <IntroMission />
       <DeepFakeCases />
       <WhatWeOffer />
       <OurTeam />
-      <Footer />
-    </div>
+    </>
+  );
+}
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/results" element={<ResultPage />} />
+        </Routes>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 
--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,9 +1,12 @@
 import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import "./HeroSection.css";
 
 function HeroSection() {
   const [caption, setCaption] = useState("");
+  const [fileName, setFileName] = useState(""); // State to hold the selected file name
   const fullCaption = "Revealing Reality, Defending Integrity";
+  const navigate = useNavigate();
 
   useEffect(() => {
     let index = 0;
@@ -17,6 +20,20 @@ function HeroSection() {
     }, 100);
     return () => clearInterval(typewriter);
   }, []);
+
+  const handleFileChange = (event) => {
+    const file = event.target.files[0];
+    if (file) {
+      setFileName(file.name); // Update the file name state
+    } else {
+      setFileName(""); // Clear the file name if no file is selected
+    }
+  };
+
+  const handleUploadClick = () => {
+    // Navigate to the results page
+    navigate("/results");
+  };
 
   return (
     <div className="hero-section" data-aos="fade-up">
@@ -33,12 +50,16 @@ function HeroSection() {
         <br></br>
         <br></br>
         <div className="upload-box">
-          <input
-            type="file"
-            className="upload-input"
-            placeholder="Upload your file..."
-          />
-          <button className="upload-button">
+          <label className="custom-file-upload">
+            <input
+              type="file"
+              className="upload-input"
+              onChange={handleFileChange}
+            />
+            <i className="fa fa-cloud-upload"></i> Choose File
+          </label>
+          {fileName && <span className="file-name">{fileName}</span>}
+          <button className="upload-button" onClick={handleUploadClick}>
             <i className="fa fa-upload"></i>
           </button>
         </div>
